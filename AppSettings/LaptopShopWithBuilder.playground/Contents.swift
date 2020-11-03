aa821d926d1158ccea7246a8e7ac4ecaea20fadf
Benson <benson.rukantabula@getinge.com>
2020-11-03 10:38:25 +0100

    Builder pattern in finalized

--- AppSettings/LaptopShopWithBuilder.playground/Contents.swift
+++ AppSettings/LaptopShopWithBuilder.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var str = "Hello, playground"
+func createBuilder(configuration: [String: String]) -> LaptopBuilder? {
+    guard let model = configuration["base_model"] else {
+        return nil
+    }
+    
+    var result: LaptopBuilder?
+    
+    switch model {
+    case "budget":
+        result = BudgetLaptopBuilder()
+    case "office":
+        result = OfficeLaptopBuilder()
+    case "high-end":
+        result = HighEndLaptopBuilder()
+    default:
+        print("Unexpected Value")
+    }
+    
+    return result
+}
+
+var configuration = ["base_model": "budget"]
+var laptopBuilder = createBuilder(configuration: configuration)
+var director = Director(builder: laptopBuilder)
+
+director.constructLaptop(configuration: configuration)
+
+if let laptop = laptopBuilder?.getLaptop(){
+    print(laptop)
+}
+
+
